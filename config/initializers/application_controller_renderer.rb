00ed555cdf568e09b330c9455826a4fc1d54ad42
maryoryo <liyomaxxde@gmail.com>
2021-05-15 02:50:03 +0000

    meshiterro

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '12c9b709aee6c3bae3a7427e18db5ad15a5c6d255805059d5eddfe5747e2a5db12b0e7e947aeff4acc09d4d1bf83150035ec7324d40def82c1f78eec342226c1'
